79f08b2f5498ac71dd53ef1e883fe38f3c4594e6
LeonardosGitHub <nandosimon@gmail.com>
2019-09-04 09:37:05 -0400

    various updates

--- ohio.tf
+++ ohio.tf
@@ -15,13 +15,13 @@ resource "aws_vpc" "vpc_gslb_ohio" {
   enable_classiclink   = "false"
 
   tags = {
-    Name = "vpc_${var.tldn_cluster_name}_gslb_ohio"
+    Name = "${var.tldn_cluster_name}_gslb_ohio"
   }
 }
 
-############################
-# BUILDING SUBNETS FOR VPC #
-############################
+###########################
+# BUILDING SUBNET FOR VPC #
+###########################
 
 resource "aws_subnet" "f5_subnet_a" {
   vpc_id                  = aws_vpc.vpc_gslb_ohio.id
@@ -62,5 +62,56 @@ resource "aws_route_table" "rt1" {
 resource "aws_main_route_table_association" "association_subnet" {
   vpc_id         = aws_vpc.vpc_gslb_ohio.id
   route_table_id = aws_route_table.rt1.id
+}
+
+###########################
+# BUILDING SECURITY GROUP #
+###########################
+
+resource "aws_security_group" "bigipexternalsecuritygroup" {
+  name   = "${aws_vpc.vpc_gslb_ohio.tags.Name}_bigip_SecGroupExternal"
+  vpc_id = aws_vpc.vpc_gslb_ohio.id
+
+  #bigipExternalSecurityGroup
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["65.60.250.160/32"]
+  }
+
+  ingress {
+    from_port   = 8443
+    to_port     = 8443
+    protocol    = "tcp"
+    cidr_blocks = ["65.60.250.160/32"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+  tags = {
+    Name = "${aws_vpc.vpc_gslb_ohio.tags.Name}_bigip_SecGroupExternal"
+  }
+}
+
+########################
+# BUILDING F5 INSTANCE #
+########################
+
+
+resource "aws_instance" "f5_bigip" {
+  ami                         = "ami-0842ee864d1e460bf"
+  instance_type               = "m5.large"
+  subnet_id                   = aws_subnet.f5_subnet_a.id                          #(Optional) The VPC Subnet ID to launch in.
+  associate_public_ip_address = true                                               #(Optional) Associate a public ip address with an instance in a VPC. Boolean value.
+  key_name                    = "f5_aws_acct_keypair"                              #(Optional) The key name of the Key Pair to use for the instance
+  vpc_security_group_ids      = [aws_security_group.bigipexternalsecuritygroup.id] #(Optional, VPC only) A list of security group IDs to associate with.
+
+  #private_ip = "" (Optional) Private IP address to associate with the instance in a VPC.
+  #availability_zone = "" (Optional) The AZ to start the instance in.
+  tags = {
+    Name = "${aws_vpc.vpc_gslb_ohio.tags.Name}_bigip_instance"
+  }
 }
 
--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,43 @@
 
+output "vpc_name" {
+  value = aws_vpc.vpc_gslb_ohio.tags.Name
+}
 output "vpc_id" {
   value = aws_vpc.vpc_gslb_ohio.id
 }
 
-output "vpc_subnet_a_cidr_block" {
-  value = aws_subnet.f5_subnet_a.cidr_block
+
+output "vpc_subnet_a_name" {
+  value = aws_subnet.f5_subnet_a.tags.Name
 }
 output "vpc_subnet_a_id" {
   value = aws_subnet.f5_subnet_a.id
+}
+output "vpc_subnet_a_cidr_block" {
+  value = aws_subnet.f5_subnet_a.cidr_block
+}
+
+output "security_group_name" {
+  value = aws_security_group.bigipexternalsecuritygroup.tags.Name
+}
+output "security_group_id" {
+  value = aws_security_group.bigipexternalsecuritygroup.id
+}
+
+output "BIG-IP_name" {
+  value = aws_instance.f5_bigip.tags.Name
+}
+
+output "BIG-IP_DNS_name" {
+  value = aws_instance.f5_bigip.public_dns
+}
+
+output "BIG-IP_public_IPaddr" {
+  value = aws_instance.f5_bigip.public_ip
+}
+
+output "BIG-IP_private_IPaddr" {
+  value = aws_instance.f5_bigip.private_ip
 }
 
 /*
