f3b04796c0266a2f0e52b7430c58a155a2d2aac5
LeonardosGitHub <nandosimon@gmail.com>
2019-09-05 12:36:10 -0400

    update

--- dc2/gslb_bigip.tf
+++ dc2/gslb_bigip.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "us-east-2"
+  region = "${var.aws_region}"
 }
 
 ########################################
@@ -7,15 +7,15 @@ provider "aws" {
 ########################################
 
 
-resource "aws_vpc" "vpc_gslb_ohio" {
-  cidr_block           = "10.0.0.0/16"
+resource "aws_vpc" "gslb_vpc" {
+  cidr_block           = "10.0.101.0/24"
   instance_tenancy     = "default"
   enable_dns_support   = "true"
   enable_dns_hostnames = "true"
   enable_classiclink   = "false"
 
   tags = {
-    Name = "${var.tldn_cluster_name}_gslb_ohio"
+    Name = "${var.tldn_cluster_name}_gslb_${var.dcX}"
   }
 }
 
@@ -24,13 +24,13 @@ resource "aws_vpc" "vpc_gslb_ohio" {
 ###########################
 
 resource "aws_subnet" "f5_subnet_a" {
-  vpc_id                  = aws_vpc.vpc_gslb_ohio.id
-  cidr_block              = "10.0.101.0/24"
+  vpc_id                  = aws_vpc.gslb_vpc.id
+  cidr_block              = "10.0.101.0/25"
   map_public_ip_on_launch = "true"
   availability_zone       = "${var.aws_region}a"
 
   tags = {
-    Name = "${aws_vpc.vpc_gslb_ohio.tags.Name}_subnet_a"
+    Name = "${aws_vpc.gslb_vpc.tags.Name}_subnet_a"
   }
 }
 
@@ -39,15 +39,15 @@ resource "aws_subnet" "f5_subnet_a" {
 ######################################
 
 resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.vpc_gslb_ohio.id
+  vpc_id = aws_vpc.gslb_vpc.id
 
   tags = {
-    Name = "${aws_vpc.vpc_gslb_ohio.tags.Name}_internet_gateway"
+    Name = "${aws_vpc.gslb_vpc.tags.Name}_internet_gateway"
   }
 }
 
 resource "aws_route_table" "rt1" {
-  vpc_id = aws_vpc.vpc_gslb_ohio.id
+  vpc_id = aws_vpc.gslb_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
@@ -55,12 +55,12 @@ resource "aws_route_table" "rt1" {
   }
 
   tags = {
-    Name = "${aws_vpc.vpc_gslb_ohio.tags.Name}_DefaultRoute"
+    Name = "${aws_vpc.gslb_vpc.tags.Name}_DefaultRoute"
   }
 }
 
 resource "aws_main_route_table_association" "association_subnet" {
-  vpc_id         = aws_vpc.vpc_gslb_ohio.id
+  vpc_id         = aws_vpc.gslb_vpc.id
   route_table_id = aws_route_table.rt1.id
 }
 
@@ -69,8 +69,8 @@ resource "aws_main_route_table_association" "association_subnet" {
 ###########################
 
 resource "aws_security_group" "bigipexternalsecuritygroup" {
-  name   = "${aws_vpc.vpc_gslb_ohio.tags.Name}_bigip_SecGroupExternal"
-  vpc_id = aws_vpc.vpc_gslb_ohio.id
+  name   = "${aws_vpc.gslb_vpc.tags.Name}_bigip_SecGroupExternal"
+  vpc_id = aws_vpc.gslb_vpc.id
 
   #bigipExternalSecurityGroup
   ingress {
@@ -91,7 +91,7 @@ resource "aws_security_group" "bigipexternalsecuritygroup" {
     create_before_destroy = true
   }
   tags = {
-    Name = "${aws_vpc.vpc_gslb_ohio.tags.Name}_bigip_SecGroupExternal"
+    Name = "${aws_vpc.gslb_vpc.tags.Name}_bigip_SecGroupExternal"
   }
 }
 
@@ -101,17 +101,17 @@ resource "aws_security_group" "bigipexternalsecuritygroup" {
 
 
 resource "aws_instance" "f5_bigip" {
-  ami                         = "ami-0842ee864d1e460bf"
+  ami                         = "${var.ami}"
   instance_type               = "m5.large"
   subnet_id                   = aws_subnet.f5_subnet_a.id                          #(Optional) The VPC Subnet ID to launch in.
   associate_public_ip_address = true                                               #(Optional) Associate a public ip address with an instance in a VPC. Boolean value.
-  key_name                    = "f5_aws_acct_keypair"                              #(Optional) The key name of the Key Pair to use for the instance
+  key_name                    = "${var.key_pair}"                                  #(Optional) The key name of the Key Pair to use for the instance
   vpc_security_group_ids      = [aws_security_group.bigipexternalsecuritygroup.id] #(Optional, VPC only) A list of security group IDs to associate with.
 
   #private_ip = "" (Optional) Private IP address to associate with the instance in a VPC.
   #availability_zone = "" (Optional) The AZ to start the instance in.
   tags = {
-    Name = "${aws_vpc.vpc_gslb_ohio.tags.Name}_bigip_instance"
+    Name = "${aws_vpc.gslb_vpc.tags.Name}_bigip_instance"
   }
 }
 
